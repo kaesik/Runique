e9c1cbe03152278556c219cd1b49e2f5b3c16f99
kaesik <108717713+kaesik@users.noreply.github.com>
2024-10-02 22:04:04 +0200

    creating string resource management

--- core/domain/src/main/java/com/kaesik/core/domain/util/Error.kt
+++ core/domain/src/main/java/com/kaesik/core/domain/util/Error.kt
@@ -1,19 +1,3 @@
 package com.kaesik.core.domain.util
 
-interface Error {
-    enum class Network: DataError{
-        REQUEST_TIMEOUT,
-        UNAUTHORIZED,
-        CONFLICT,
-        TOO_MANY_REQUESTS,
-        NO_INTERNET,
-        PAYLOAD_TOO_LARGE,
-        SERVER_ERROR,
-        SERIALIZATION,
-        UNKNOWN
-    }
-
-    enum class Local: DataError{
-        DISK_FULL
-    }
-}
+interface Error
---
+++ core/presentation/ui/src/main/java/com/kaesik/core/presentation/ui/DataErrorToText.kt
@@ -0,0 +1,26 @@
+package com.kaesik.core.presentation.ui
+
+import com.kaesik.core.domain.util.DataError
+
+fun DataError.asUiText(): UiText {
+    return when (this) {
+        DataError.Local.DISK_FULL ->
+            UiText.StringResource(id = R.string.error_disk_full)
+        DataError.Network.REQUEST_TIMEOUT ->
+            UiText.StringResource(id = R.string.error_request_timeout)
+        DataError.Network.TOO_MANY_REQUESTS ->
+            UiText.StringResource(id = R.string.error_too_many_requests)
+        DataError.Network.NO_INTERNET ->
+            UiText.StringResource(id = R.string.error_no_internet)
+        DataError.Network.PAYLOAD_TOO_LARGE ->
+            UiText.StringResource(id = R.string.error_payload_too_large)
+        DataError.Network.SERVER_ERROR ->
+            UiText.StringResource(id = R.string.error_server_error)
+        DataError.Network.SERIALIZATION ->
+            UiText.StringResource(id = R.string.error_serialization)
+        else ->
+            UiText.StringResource(id = R.string.error_unknown)
+    }
+
+
+}
