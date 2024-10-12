6633ad03528b5f76dcb76c3fa161e4373c6a0ed4
kaesik <108717713+kaesik@users.noreply.github.com>
2024-10-12 22:04:46 +0200

    setting up session storage

---
+++ core/data/src/main/java/com/kaesik/core/data/auth/AuthInfoMapper.kt
@@ -0,0 +1,19 @@
+package com.kaesik.core.data.auth
+
+import com.kaesik.core.domain.AuthInfo
+
+fun AuthInfo.toAuthInfoSerializable(): AuthInfoSerializable {
+    return AuthInfoSerializable(
+        accessToken = accessToken,
+        refreshToken = refreshToken,
+        userId = userId
+    )
+}
+
+fun AuthInfoSerializable.toAuthInfo(): AuthInfo {
+    return AuthInfo(
+        accessToken = accessToken,
+        refreshToken = refreshToken,
+        userId = userId
+    )
+}
--- core/data/src/main/java/com/kaesik/core/data/di/CoreDataModule.kt
+++ core/data/src/main/java/com/kaesik/core/data/di/CoreDataModule.kt
@@ -1,10 +1,15 @@
 package com.kaesik.core.data.di
 
+import com.kaesik.core.data.auth.EncryptedSessionStorage
 import com.kaesik.core.data.networking.HttpClientFactory
+import com.kaesik.core.domain.SessionStorage
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val coreDataModule = module {
     single {
         HttpClientFactory().build()
     }
+    singleOf(::EncryptedSessionStorage).bind<SessionStorage>()
 }
