51ed2344d1731a720a4ac9ca2127ac1c5f928955
kaesik <108717713+kaesik@users.noreply.github.com>
2024-11-06 20:51:41 +0100

    setting up the RunActiveViewModel

--- run/presentation/src/main/java/com/kaesik/run/presentation/run_active/RunActiveViewModel.kt
+++ run/presentation/src/main/java/com/kaesik/run/presentation/run_active/RunActiveViewModel.kt
@@ -3,15 +3,19 @@ package com.kaesik.run.presentation.run_active
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.kaesik.run.domain.RunningTracker
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.receiveAsFlow
-import timber.log.Timber
+import kotlinx.coroutines.flow.stateIn
 
 class RunActiveViewModel(
     private val runningTracker: RunningTracker
@@ -22,10 +26,18 @@ class RunActiveViewModel(
     private val eventChannel = Channel<RunActiveEvent>()
     val events = eventChannel.receiveAsFlow()
 
-    private val _hasLocationPermission = MutableStateFlow(false)
+    private val shouldTrack = snapshotFlow { state.shouldTrack }
+        .stateIn(viewModelScope, SharingStarted.Lazily, state.shouldTrack)
+    private val hasLocationPermission = MutableStateFlow(false)
+    private val isTracking = combine(
+        shouldTrack,
+        hasLocationPermission
+    ) { shouldTrack, hasPermission ->
+        shouldTrack && hasPermission
+    }.stateIn(viewModelScope, SharingStarted.Lazily, false)
 
     init {
-        _hasLocationPermission
+        hasLocationPermission
             .onEach { hasPermission ->
                 if (hasPermission) {
                     runningTracker.startObservingLocation()
@@ -35,12 +47,33 @@ class RunActiveViewModel(
             }
             .launchIn(viewModelScope)
 
-        runningTracker
-            .currentLocation
-            .onEach { location ->
-                Timber.d("New location: $location")
+        isTracking
+            .onEach { isTracking ->
+                runningTracker.setIsTracking(isTracking)
             }
             .launchIn(viewModelScope)
+
+        runningTracker
+            .currentLocation
+            .onEach {
+                state = state.copy(currentRuniqueLocation = it)
+            }
+            .launchIn(viewModelScope)
+
+        runningTracker
+            .runData
+            .onEach {
+                state = state.copy(runData = it)
+            }
+            .launchIn(viewModelScope)
+
+        runningTracker
+            .elapsedTime
+            .onEach {
+                state = state.copy(elapsedTime = it)
+            }
+            .launchIn(viewModelScope)
+
     }
 
     fun onAction(action: RunActiveAction) {
@@ -48,11 +81,23 @@ class RunActiveViewModel(
             RunActiveAction.OnFinishRunClick -> {
             }
             RunActiveAction.OnResumeRunClick -> {
+                state = state.copy(
+                    shouldTrack = true
+                )
+            }
+            RunActiveAction.OnBackClick -> {
+                state = state.copy(
+                    shouldTrack = false
+                )
             }
             RunActiveAction.OnToggleRunClick -> {
+                state = state.copy(
+                    hasStartedRunning = true,
+                    shouldTrack = !state.shouldTrack
+                )
             }
             is RunActiveAction.SubmitLocationPermissionInfo -> {
-                _hasLocationPermission.value = action.acceptedLocationPermission
+                hasLocationPermission.value = action.acceptedLocationPermission
                 state = state.copy(
                     showLocationRationale = action.showLocationRationale
                 )
